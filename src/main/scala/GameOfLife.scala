25027a8bf8015124711edc410d34095a666ecdb5
Marco Morado <marco.morado@pacsis.pt>
2016-09-29 04:44:48 +0100

    Implementation of neighbours and next generation

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,3 +1,4 @@
+import scala.math._
 import scala.util.Properties._
 
 object GameOfLife {
@@ -18,6 +19,43 @@ object GameOfLife {
     def awake(cs: (Int, Int)*) = this.copy(alive = alive ++ cs)
 
     def kill(cs: (Int, Int)*) = this.copy(alive = alive diff cs)
+
+    def liveNeighbours(row: Int, col: Int) = {
+      def notMe(r: Int, c: Int) = (r, c) != (row + 1, col + 1)
+
+      def nextToMe(r: Int, c: Int) = abs(row + 1 - r) <= 1 && abs(col + 1 - c) <= 1
+
+      def copyRow(s: Seq[(Int, Int)], from: Int, to: Int) =
+        s filter { case (r, c) => r == from } map { case (r, c) => (to, c) }
+
+      def copyCol(s: Seq[(Int, Int)], from: Int, to: Int) =
+        s filter { case (r, c) => c == from } map { case (r, c) => (r, to) }
+
+      val shifted = alive.map(t => (t._1 + 1, t._2 + 1))
+      val large = shifted ++
+        copyRow(shifted, rows, 0) ++
+        copyRow(shifted, 0, rows) ++
+        copyCol(shifted, cols, 0) ++
+        copyCol(shifted, 0, cols)
+
+      large.count { case (r, c) => notMe(r, c) && nextToMe(r, c) }
+    }
+
+    def next = {
+      def isAlive(r: Int, c: Int) = alive.contains((r, c))
+
+      val matrix = for {
+        r <- 0 until rows
+        c <- 0 until cols
+      } yield (r, c)
+
+      val newAlive = matrix.filter { case (r, c) =>
+        val count = liveNeighbours(r, c)
+        count == 3 || isAlive(r, c) && count == 2
+      }
+
+      copy(alive = newAlive)
+    }
 
     override def toString: String = {
       def xo(c: (Int, Int)) = if (alive.contains(c)) "O" else "X"
--- src/test/scala/GameOfLifeSpec.scala
+++ src/test/scala/GameOfLifeSpec.scala
@@ -93,6 +93,70 @@ class GameOfLifeSpec extends BaseSpec {
       result.toString shouldBe """XXO
                                  |XXX""".stripMargin
     }
+
+    "return all the neighbours of cell given by position w/out wrap" in {
+      // X X O X
+      // O[O]O X
+      // X O X O
+      val board = Board(3, 4)
+        .awake((1,1)) // self, excluded
+        .awake((1,2), (1,0), (0,2), (2, 1))
+        .awake((2,3)) // excluded
+
+      val result = board.liveNeighbours(1, 1)
+
+      result shouldBe 4
+    }
+
+    "return all the neighbours of cell given by position with wrap" in {
+      //[X]X X O
+      // O X O O
+      // X O X X
+      val board = Board(3, 4)
+        .awake((1,3), (1,0), (0,3), (2, 1))
+        .awake((1,2)) // excluded
+
+      val result = board.liveNeighbours(0, 0)
+
+      result shouldBe 4
+    }
+
+    "wrap around the edges" in {
+      // X X O
+      // O X O
+      // X X X
+      val board = Board(3, 3).awake((1,2), (1,0), (0,2))
+
+      val result = board.next
+
+      result.toString shouldBe """XOO
+                                 |OOO
+                                 |XOX""".stripMargin
+      /*
+         X   X X X   X
+
+         O   X X O   X
+         O   O X O   O
+         X   X X X   X
+
+         O   X X O   X
+      */
+      /*
+         X   X X X   X
+
+         O   O O O   X
+         O   O O O   O
+         X   X X O   X
+
+         O   X X O   X
+      */
+
+      """
+        |OOX
+        |OOX
+        |XXX
+      """.stripMargin
+    }
   }
 }
 
